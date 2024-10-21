57a48496f73c2021b6c2c2daa9893ad8cd0b49d6
Zgrovetin <534477@gmail.com>
2024-10-22 00:35:32 +0200

    So and redirection from main domain to jobs

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
+Route::get('', fn() => to_route('jobs.index'));
 Route::resource('jobs', JobController::class)->only(['index']);
